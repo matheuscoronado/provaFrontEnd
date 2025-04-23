4e23320279fd8dd731748fa8a050b69be95a0979
Matheus Coronado <matheuscoronado48@gmail.com>
2025-04-22 21:22:02 -0300

    Criado função para preencher a lista

--- script.js
+++ script.js
@@ -2,14 +2,43 @@
 var dadosLista = [];
 
 function salvarDados(){
-    let Descricao = document.getElementById("nameUser").value; // Pega o valor do campo de texto
+    let Descricao = document.getElementById("descricao").value; // Pega o valor do campo de texto
     if(Descricao == ""){ // Verifica se o campo está vazio
         alert("Campo vazio!"); // Alerta o usuário
             return; // Retorna para não executar o restante do código
         } else {
             dadosLista.push(Descricao); // Adiciona o valor do campo de texto no array
-            document.getElementById("nameUser").value = ""; // Limpa o campo de texto
+            document.getElementById("descricao").value = ""; // Limpa o campo de texto
             console.log(dadosLista); // Mostra o array atualizado no console
             criaLista(); // Chama a função para preencher a lista de cadastro
         }
+    let Valor = document.getElementById("valor").value; // Pega o valor do campo de texto
+    if(Valor == ""){ // Verifica se o campo está vazio
+        alert("Campo vazio!"); // Alerta o usuário
+            return; // Retorna para não executar o restante do código
+        } else {
+            dadosLista.push(Valor); // Adiciona o valor do campo de texto no array
+            document.getElementById("valor").value = ""; // Limpa o campo de texto
+            console.log(dadosLista); // Mostra o array atualizado no console
+            criaLista(); // Chama a função para preencher a lista de cadastro
+        }
+    let Categoria = document.getElementById("categoria").value; // Pega o valor do campo de texto
+    if(Categoria == ""){ // Verifica se o campo está vazio
+        alert("Campo vazio!"); // Alerta o usuário
+            return; // Retorna para não executar o restante do código
+        } else {
+            dadosLista.push(Categoria); // Adiciona o valor do campo de texto no array
+            document.getElementById("categoria").value = ""; // Limpa o campo de texto
+            console.log(dadosLista); // Mostra o array atualizado no console
+            criaLista(); // Chama a função para preencher a lista de cadastro
+        }
+}
+// Função para preencher a lista de cadastro
+
+function criaLista(){
+    let tabela = "<tr><td>Descrição</th><th>Valor</td><td>Categoria</th></tr>";
+    for(let i = 0; i <= dadosLista.length-1; i++){
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='limparTabela(this.parentNode.parentNode.rowIndex)'>limpar Lista</button><button' onclick='adicionar(this.parentNode.parentNode.rowIndex)'>adicionar</button></td></tr>";
+        document.getElementById("tabela").innerHTML = tabela;
+    }
 }
